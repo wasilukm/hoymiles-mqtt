d8b1c6cf81a5e2cd43f12cf92f8bd7e0258a57a5
Marek Majchrowski <Marek.Majchrowski@intuitionmachines.com>
2024-02-05 22:08:38 +0100

    Support for MQTT TLS connection

--- hoymiles_mqtt/mqtt.py
+++ hoymiles_mqtt/mqtt.py
@@ -1,11 +1,21 @@
 """MQTT related interfaces."""
+import ssl
+
 from paho.mqtt.publish import single as publish_single
 
 
 class MqttPublisher:
     """MQTT Publisher."""
 
-    def __init__(self, mqtt_broker: str, mqtt_port: int, mqtt_user: str = None, mqtt_password: str = None):
+    def __init__(
+        self,
+        mqtt_broker: str,
+        mqtt_port: int,
+        mqtt_user: str = None,
+        mqtt_password: str = None,
+        mqtt_tls: bool = False,
+        mqtt_tls_insecure: bool = False,
+    ):
         """Initialize the object.
 
         Arguments:
@@ -13,6 +23,8 @@ class MqttPublisher:
             mqtt_port: port of MQTT broker
             mqtt_user: MQTT username
             mqtt_password: password
+            mqtt_tls: TLS connection
+            mqtt_tls_insecure: TLS insecure connection
 
         """
         self._mqtt_broker = mqtt_broker
@@ -20,6 +32,11 @@ class MqttPublisher:
         self._auth = None
         if mqtt_user and mqtt_password:
             self._auth = {'username': mqtt_user, 'password': mqtt_password}
+        self._tls = None
+        if mqtt_tls:
+            self._tls = {'tls_version': ssl.PROTOCOL_TLS_CLIENT}
+            if mqtt_tls_insecure:
+                self._tls = {'insecure': True, 'tls_version': ssl.PROTOCOL_TLS_CLIENT}
 
     def publish(self, topic: str, message: str, retain: bool = False) -> None:
         """Publish a message to the given MQTT topic.
@@ -37,4 +54,5 @@ class MqttPublisher:
             port=self._mqtt_port,
             auth=self._auth,
             retain=retain,
+            tls=self._tls,
         )
