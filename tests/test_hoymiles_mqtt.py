8cbbd83ec71bf49fd27c1d9d8a8ae9c7b7132e35
Mariusz Wasiluk <w.mariusz@gmail.com>
2024-02-12 00:43:11 +0100

    add tests

--- tests/test_hoymiles_mqtt.py
+++ tests/test_hoymiles_mqtt.py
@@ -7,31 +7,34 @@ from hoymiles_modbus.client import MISeriesMicroinverterData, PlantData
 from hoymiles_mqtt import MI_ENTITIES, PORT_ENTITIES
 from hoymiles_mqtt.ha import HassMqtt
 
-example_microinverter_data = MISeriesMicroinverterData(
-    data_type=0,
-    serial_number='102162804827',
-    port_number=3,
-    pv_voltage=1.234,
-    pv_current=2.34,
-    grid_voltage=22.33,
-    grid_frequency=32.12,
-    pv_power=40.31,
-    today_production=431,
-    total_production=8844,
-    temperature=20.4,
-    operating_status=3,
-    alarm_code=0,
-    alarm_count=2,
-    link_status=1,
-    reserved=[],
-)
-example_plant_data = PlantData('dtu_serial', microinverter_data=[example_microinverter_data])
+
+def get_example_data() -> PlantData:
+    """Create example PlantData."""
+    example_microinverter_data = MISeriesMicroinverterData(
+        data_type=0,
+        serial_number='102162804827',
+        port_number=3,
+        pv_voltage=1.234,
+        pv_current=2.34,
+        grid_voltage=22.33,
+        grid_frequency=32.12,
+        pv_power=40.31,
+        today_production=431,
+        total_production=8844,
+        temperature=20.4,
+        operating_status=3,
+        alarm_code=0,
+        alarm_count=2,
+        link_status=1,
+        reserved=[],
+    )
+    return PlantData('dtu_serial', microinverter_data=[example_microinverter_data])
 
 
 def test_config_payload():
     """Test HassMqtt.config_payload."""
     ha = HassMqtt(mi_entities=['grid_voltage'], port_entities=['pv_voltage'])
-    payload = list(ha.get_configs(example_plant_data))
+    payload = list(ha.get_configs(get_example_data()))
     assert payload[0] == (
         'homeassistant/sensor/dtu_serial/DTU_pv_power/config',
         json.dumps(
@@ -161,7 +164,8 @@ def test_config_payload():
 def test_get_states():
     """Test HassMqtt.get_states."""
     ha = HassMqtt(mi_entities=MI_ENTITIES, port_entities=PORT_ENTITIES)
-    states = list(ha.get_states(example_plant_data))
+    example_data = get_example_data()
+    states = list(ha.get_states(example_data))
     assert states[0] == (
         'homeassistant/hoymiles_mqtt/dtu_serial/state',
         '{"pv_power": 0.0, "today_production": 431, "total_production": 8844, "alarm_flag": "OFF"}',
@@ -170,6 +174,53 @@ def test_get_states():
         'homeassistant/hoymiles_mqtt/102162804827/state',
         '{"grid_voltage": 22.33, "grid_frequency": 32.12, "temperature": 20.4, "operating_status": 3, '
         '"alarm_code": 0, "alarm_count": 2, "link_status": 1}',
+    )
+    assert states[2] == (
+        'homeassistant/hoymiles_mqtt/102162804827/3/state',
+        '{"pv_voltage": 1.234, "pv_current": 2.34, "pv_power": 40.31, "today_production": 431, '
+        '"total_production": 8844}',
+    )
+    example_data.microinverter_data[0].today_production += 1
+    example_data.microinverter_data[0].total_production += 2
+    states = list(ha.get_states(example_data))
+    assert states[0] == (
+        'homeassistant/hoymiles_mqtt/dtu_serial/state',
+        '{"pv_power": 0.0, "today_production": 432, "total_production": 8846, "alarm_flag": "OFF"}',
+    )
+    assert states[2] == (
+        'homeassistant/hoymiles_mqtt/102162804827/3/state',
+        '{"pv_voltage": 1.234, "pv_current": 2.34, "pv_power": 40.31, "today_production": 432, '
+        '"total_production": 8846}',
+    )
+
+
+def test_zero_operating_status():
+    """Verify that data is not updated when operating status is zero."""
+    ha = HassMqtt(mi_entities=MI_ENTITIES, port_entities=PORT_ENTITIES)
+    example_data = get_example_data()
+    list(ha.get_states(example_data))
+
+    example_data.microinverter_data[0].operating_status = 0
+    example_data.microinverter_data[0].today_production += 1
+    example_data.microinverter_data[0].total_production += 2
+    states = list(ha.get_states(example_data))
+    assert states[0] == (
+        'homeassistant/hoymiles_mqtt/dtu_serial/state',
+        '{"pv_power": 0.0, "today_production": 431, "total_production": 8844, "alarm_flag": "OFF"}',
+    )
+
+
+def test_production_drop():
+    """Verify that drop in production is ignored."""
+    ha = HassMqtt(mi_entities=MI_ENTITIES, port_entities=PORT_ENTITIES)
+    example_data = get_example_data()
+    list(ha.get_states(example_data))
+    example_data.microinverter_data[0].today_production -= 1
+    example_data.microinverter_data[0].total_production -= 2
+    states = list(ha.get_states(example_data))
+    assert states[0] == (
+        'homeassistant/hoymiles_mqtt/dtu_serial/state',
+        '{"pv_power": 0.0, "today_production": 431, "total_production": 8844, "alarm_flag": "OFF"}',
     )
     assert states[2] == (
         'homeassistant/hoymiles_mqtt/102162804827/3/state',
